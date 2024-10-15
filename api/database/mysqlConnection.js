21ecd8796fdf8f2e895b8468b001b76362f66150
Daber98 <dberduoh@miumg.edu.gt>
2024-10-14 22:18:03 -0600

    Cambios en package.json

--- api/database/mysqlConnection.js
+++ api/database/mysqlConnection.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect(function(err) {
